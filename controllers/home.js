1864ac9ab7e6f0fd8587648024365d10fd914c79
darshanjayaram11 <darshzz2311@gmail.com>
2026-03-17 16:34:23 +0530

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -6,4 +6,10 @@ exports.getLanding = (req, res, next) => {
   res.render('landing');
 }
 
+exports.getError403 = (req, res, next) => {
+  res.render('error403');
+}
 
+exports.getError404 = (req, res, next) => {
+  res.render('error404');
+}
